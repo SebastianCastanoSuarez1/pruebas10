b5efd678a47cfab31ed03cc71ce5b0ad4ff71560
Sebastian <samesco323@gmail.com>
2023-11-12 18:48:04 +0100

    resources

--- GestionEmpleadosHibernate/src/main/java/controller/Controller.java
+++ GestionEmpleadosHibernate/src/main/java/controller/Controller.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 import models.Departamento;
+import models.Empleado;
 import models.Proyecto;
 import repositories.departamento.Departamentorepository;
 import repositories.empleado.Empleadorepository;
@@ -23,7 +24,8 @@ public class Controller {
 		this.empleadorepository = empleadorepository;
 		this.proyectorepository = proyectorepository;
 	}
-	//Departamento
+
+	// Departamento
 	public List<Departamento> getDepartamentorepository() {
 		logger.info("Obteniendo toods los proyectos");
 		return departamentorepository.findAll();
@@ -48,9 +50,30 @@ public class Controller {
 		logger.info("eliminando el proyecto " + departamento.getId() + " con nombre " + departamento.getNombre());
 		return departamentorepository.delete(departamento);
 	}
-	//Empleado
+
+	// Empleado
 	public Empleadorepository getEmpleadorepository() {
 		return empleadorepository;
+	}
+
+	public Empleado createProyecto(Empleado empleado) {
+		logger.info("Craer empleado");
+		return empleadorepository.save(empleado);
+	}
+
+	public Optional<Proyecto> getEmpleadoById(Integer id) {
+		logger.info("Obteninedo el proyecto por el id: " + id);
+		return proyectorepository.findById(id);
+	}
+
+	public Empleado updateProyecto(Empleado empleado) {
+		logger.info("Actualizando el proyecto " + empleado.getId());
+		return empleadorepository.save(empleado);
+	}
+
+	public Boolean deleteProyecto(Empleado empleado) {
+		logger.info("eliminando el proyecto " + empleado.getId() + " con nombre " + empleado.getNombre());
+		return empleadorepository.delete(empleado);
 	}
 
 	// Proyecto
